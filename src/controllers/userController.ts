1078a6b080feff73f44fec061d5c958e435e75b2
MateusDiniz9 <mateus.09098@gmail.com>
2024-09-10 20:15:27 -0300

    add schemas to user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,17 @@
 import userService from '../services/userSevice';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { userSchema } from '../schemas/userSchemas';
 
 export async function usersPost(req: Request, res: Response) {
   const { email, password } = req.body;
 
   try {
+    const { error } = userSchema.validate({ email, password });
+    if (error) {
+      return res.status(httpStatus.BAD_REQUEST).send(error);
+    }
+
     const user = await userService.createUser({ email, password });
     return res.status(httpStatus.CREATED).json({
       id: user.id,
@@ -23,6 +29,12 @@ export async function singInPost(req: Request, res: Response) {
   const { email, password } = req.body;
 
   try {
+    const { error } = userSchema.validate({ email, password });
+
+    if (error) {
+      return res.status(httpStatus.BAD_REQUEST).send(error);
+    }
+
     const result = await userService.signIn({ email, password });
 
     return res.status(httpStatus.OK).send(result);

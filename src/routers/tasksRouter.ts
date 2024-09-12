3e3e6a14ff6acbe32d7d48ef3f10c32dc7506b9d
MateusDiniz9 <mateus.09098@gmail.com>
2024-09-11 22:40:09 -0300

    fix:validate taskschema

--- src/routers/tasksRouter.ts
+++ src/routers/tasksRouter.ts
@@ -1,11 +1,13 @@
 import { authenticateToken } from '../middlewares/authenticationMiddleware';
 import { getTasks, taskPost, taskPut, taskRemove } from '../controllers/tasksController';
 import { Router } from 'express';
+import validateSchema from '../middlewares/validationMiddleware';
+import { taskSchema } from '../schemas/taskSchemas';
 
 const tasksRouter = Router();
 
 tasksRouter.get('/', authenticateToken, getTasks);
-tasksRouter.post('/', authenticateToken, taskPost);
+tasksRouter.post('/', authenticateToken, validateSchema(taskSchema), taskPost);
 tasksRouter.put('/:id', authenticateToken, taskPut);
 tasksRouter.delete('/:id', authenticateToken, taskRemove);
 
--- src/schemas/taskSchemas.ts
+++ src/schemas/taskSchemas.ts
@@ -1,8 +1,9 @@
 import Joi from 'joi';
 
 export const taskSchema = Joi.object({
-  userId: Joi.string().required(),
+  userId: Joi.required(),
   title: Joi.string().required(),
   description: Joi.string().required(),
+  status: Joi.string().required(),
   priority: Joi.string().required(),
 });
